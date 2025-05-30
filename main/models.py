7592b0e002a02591e8d53a64cd3b561251d1e41b
blackSkr <briliansatria2@gmail.com>
2025-05-31 05:17:04 +0800

    fix: memperbaiki models

--- main/models.py
+++ main/models.py
@@ -7,3 +7,19 @@ class KategoriSoal(models.Model):
 
     def __str__(self):
         return self.nama
+
+class Soal(models.Model):
+    kategoriSoal = models.ForeignKey(KategoriSoal, on_delete = models.CASCADE,related_name = 'katSoal')
+    nama = models.CharField(max_length=50,blank=False, null=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nama
+
+class JawabanSoal(models.Model):
+    soal = models.ForeignKey(Soal, on_delete = models.CASCADE,related_name = 'soal')  
+    kunci = models.CharField(max_length=50,blank=False, null=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.kunci
